e9bdaeb4e96a2b7b216c880e106f65e8fc4d97ce
alan_omam <omamalan@hotmail.it>
2024-02-13 15:46:10 +0100

    fatto bonus 1

--- index.js
+++ index.js
@@ -9,46 +9,46 @@ let team = [
 {
     nome: "wayne Barnet",
     ruolo: "Founder & ceo",
-    foto: "barnett-founder-ceo.jpg"
+    foto: "img/wayne-barnett-founder-ceo.jpg"
 },
 
 {
     nome: "Angela Caroll",
     ruolo: "Chief Editor",
-    foto: "angela-caroll-chief-editor.jpg"
+    foto: "img/angela-caroll-chief-editor.jpg"
 },
 
 {
     nome: "Walter Gordon",
     ruolo: "Office Manager",
-    foto: "walter-gordon-office-manager.jpg"
+    foto: "img/walter-gordon-office-manager.jpg"
 },
 
 {
     nome: "Angela Lopez",
     ruolo: "Social Media Manager",
-    foto: "angela-lopez-social-media-manager.jpg"
+    foto: "img/angela-lopez-social-media-manager.jpg"
 },
 
 {
     nome: "Scott Estrada",
     ruolo: "Developer",
-    foto: "estrada-developer.jpg"
+    foto: "img/scott-estrada-developer.jpg"
 },
 
 {
     nome: "Barbara Ramos",
     ruolo: "Graphic Designer",
-    foto: "barbara-ramos-graphic-designer.jpg"
+    foto: "img/barbara-ramos-graphic-designer.jpg"
 },
 
 ]
 
-//console.log(team)
+ //console.log(team[1].foto)
 
 for(let i= 0; i<team.length; i++){
    // console.log(team[i])
-    lista.innerHTML += `<li>${team[i].nome} ; ${team[i].ruolo} ; ${team[i].foto}</li>`
+    lista.innerHTML += `<li>${team[i].nome} ; ${team[i].ruolo} ;<img src= ${team[i].foto} alt="image"></li>`
 }
 
 
