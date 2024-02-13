da9410869c071e96b779ec3dd0d21ee901f5f95c
alan_omam <omamalan@hotmail.it>
2024-02-13 15:08:12 +0100

    impostato array di oggetti

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 /* 
 
 */
+
+
+let team = [
+
+{
+    nome: "wayne Barnet",
+    ruolo: "Founder & ceo",
+    foto: "barnett-founder-ceo.jpg"
+},
+
+{
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    foto: "angela-caroll-chief-editor.jpg"
+},
+
+{
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    foto: "walter-gordon-office-manager.jpg"
+},
+
+{
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    foto: "angela-lopez-social-media-manager.jpg"
+},
+
+{
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    foto: "estrada-developer.jpg"
+},
+
+{
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    foto: "barbara-ramos-graphic-designer.jpg"
+},
+
+]
+
+
+
